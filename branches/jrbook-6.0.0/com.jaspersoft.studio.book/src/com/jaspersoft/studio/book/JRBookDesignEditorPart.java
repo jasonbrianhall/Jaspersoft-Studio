21fd3cc23f6fa5eba9403edd786b42af737eb507
mrabbi <mrabbi@6833c83e-4fd0-4c68-a3df-283c6148db30>
2014-10-21 12:42:38 +0000

    JasperReports Book plug-in.

---
+++ branches/jrbook-6.0.0/com.jaspersoft.studio.book/src/com/jaspersoft/studio/book/JRBookDesignEditorPart.java
@@ -0,0 +1,33 @@
+package com.jaspersoft.studio.book;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
+import com.jaspersoft.studio.editor.preview.ABasicEditor;
+
+public class JRBookDesignEditorPart extends ABasicEditor {
+
+	public JRBookDesignEditorPart(boolean listenResource) {
+		super(listenResource);
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		Composite cmp = new Composite(parent,SWT.NONE);
+		cmp.setLayout(new GridLayout(1,false));
+		
+		Label lbl = new Label(cmp,SWT.NONE);
+		lbl.setText("MAIN DESIGNER FOR JASPERREPORTS BOOK -- TO BE DONE!");
+		lbl.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+	}
+
+	@Override
+	public void setFocus() {
+		
+	}
+
+
+}
