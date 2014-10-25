c39f4ade4a7af112ac84fc9261f136415c910285
mrabbi <mrabbi@6833c83e-4fd0-4c68-a3df-283c6148db30>
2014-10-25 14:09:01 +0000

    Fixed some stuff related to Graphics.

--- branches/jrbook-6.0.0/com.jaspersoft.studio.book/src/com/jaspersoft/studio/book/editors/figures/PageFigure.java
+++ branches/jrbook-6.0.0/com.jaspersoft.studio.book/src/com/jaspersoft/studio/book/editors/figures/PageFigure.java
@@ -1,18 +1,13 @@
 package com.jaspersoft.studio.book.editors.figures;
 
-import net.sf.jasperreports.eclipse.ui.util.UIUtils;
-
-import org.eclipse.draw2d.Figure;
 import org.eclipse.draw2d.Graphics;
-import org.eclipse.draw2d.GridData;
-import org.eclipse.draw2d.GridLayout;
-import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.ImageFigure;
 import org.eclipse.draw2d.Label;
+import org.eclipse.draw2d.OrderedLayout;
 import org.eclipse.draw2d.PositionConstants;
 import org.eclipse.draw2d.RectangleFigure;
+import org.eclipse.draw2d.ToolbarLayout;
 import org.eclipse.draw2d.geometry.Rectangle;
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.wb.swt.ResourceManager;
 
@@ -30,26 +25,25 @@ public class PageFigure extends RectangleFigure {
 		super();
 		this.model = model;
 		setPreferredSize(150, 150);
-		setLayoutManager(new GridLayout(1, false));
+		setOutline(false);
+		ToolbarLayout layout = new ToolbarLayout(false);
+		layout.setMinorAlignment(OrderedLayout.ALIGN_CENTER);
+		layout.setSpacing(5);
+		setLayoutManager(layout);
+		setBackgroundColor(ResourceManager.getColor(255, 255, 255));
 		if (scaledImage == null){
 			Image sourceImage = ResourceManager.getImage(model.getImageDescriptor());
 			scaleImage(sourceImage, getPreferredSize().width, getPreferredSize().height-20);
 		}
 		
-		ImageFigure imageFigure = new ImageFigure(ResourceManager.getImage(model.getImageDescriptor()));
-		imageFigure.setBackgroundColor(getBackgroundColor());
+		ImageFigure imageFigure = new ImageFigure(scaledImage);
 		imageFigure.setAlignment(PositionConstants.CENTER);
-		GridData imageData = new GridData();
-		imageData.horizontalAlignment = SWT.CENTER;
-		imageData.verticalAlignment = SWT.FILL;
-		add(imageFigure, imageData);
-		setBackgroundColor(ResourceManager.getColor(255, 0, 0));
+		add(imageFigure);
 		
 		String text = model.getDisplayText();
-		GridData textData = new GridData();
-		textData.horizontalAlignment = SWT.CENTER;
-		textData.verticalAlignment = SWT.TOP;
-		add(new Label(text), textData);
+		Label textFigure = new Label(text);
+		textFigure.setTextAlignment(PositionConstants.CENTER);
+		add(textFigure);
 	
 	}
 	
@@ -58,39 +52,15 @@ public class PageFigure extends RectangleFigure {
 		super.setBounds(new Rectangle(rect.x+15, rect.y+25, rect.width, rect.height));
 	}
 	
+	 @Override
+	  protected void fillShape(Graphics graphics)
+	  {
+	    int oldAlpha = graphics.getAlpha();
 
-	@Override
-	protected void fillShape(Graphics graphics) {
-		/*Rectangle figureBounds = getBounds();
-		Rectangle imagebounds = new Rectangle(figureBounds);
-		imagebounds.setHeight(figureBounds.height - 11);
-		if (scaledImage == null){
-			scaleImage(ResourceManager.getImage(model.getImageDescriptor()), imagebounds);
-		}
-		int imageWidth = scaledImage.getImageData().width;
-		int leftMargin = Math.abs(figureBounds.width-imageWidth)/2;
-		graphics.drawImage(scaledImage, figureBounds.x+leftMargin, figureBounds.y);
-		
-		//Draw the text
-		
-		graphics.getFont().getFontData()[0].setHeight(10);
-		int charWidth = graphics.getFontMetrics().getAverageCharWidth();
-		String text = model.getDisplayText();
-		
-		int textWidth = text.length()*charWidth;
-		if (textWidth>figureBounds.width){
-			int maxCahrs = (figureBounds.width/charWidth)-1;
-			text = text.substring(text.length()-maxCahrs+3);//the plus 3 is to add the three dots
-			text = "..." + text;
-			textWidth = text.length()*charWidth;
-		}
-		int textLeftMargin = Math.abs(figureBounds.width-textWidth)/2;
-		graphics.drawText(text, figureBounds.x+textLeftMargin, figureBounds.y + figureBounds.height - 10);*/
-	}
-	
-	@Override
-	protected void outlineShape(Graphics graphics) {
-	}
+	    graphics.setAlpha(128);
+	    super.fillShape(graphics);
+	    graphics.setAlpha(oldAlpha);
+	  }
 	
 	private void scaleImage(Image sourceImage, int width, int height) {
 		int newWidth = 0;
@@ -107,4 +77,5 @@ public class PageFigure extends RectangleFigure {
 		}
 		scaledImage = new Image(null, sourceImage.getImageData().scaledTo(newWidth, newHeight));
 	}
+	
 }
