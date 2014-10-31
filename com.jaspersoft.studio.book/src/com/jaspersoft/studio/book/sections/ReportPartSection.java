2ed6dd4b3615b608096a2a71cd6e82a506185808
mrabbi <mrabbi@6833c83e-4fd0-4c68-a3df-283c6148db30>
2014-10-31 12:11:49 +0000

    Better layouting.

--- com.jaspersoft.studio.book/src/com/jaspersoft/studio/book/sections/ReportPartSection.java
+++ com.jaspersoft.studio.book/src/com/jaspersoft/studio/book/sections/ReportPartSection.java
@@ -2,6 +2,7 @@ package com.jaspersoft.studio.book.sections;
 
 import net.sf.jasperreports.engine.design.JRDesignPart;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -18,8 +19,8 @@ public class ReportPartSection extends AbstractSection {
 		super.createControls(parent, tabbedPropertySheetPage);
 
 		parent.setLayout(new GridLayout(2, false));
-		GridData comboData = new GridData();
-		comboData.widthHint = 50;
+		GridData comboData = new GridData(SWT.LEFT,SWT.FILL,false,false);
+		comboData.widthHint = 100;
 		createWidget4Property(parent, MReportPart.PROPERTY_EVALTIME_TYPE).getControl().setLayoutData(comboData);
 		createWidget4Property(parent, MReportPart.COMPONENT_EXPRESSION);
 		createWidget4Property(parent, JRDesignPart.PROPERTY_PRINT_WHEN_EXPRESSION);
